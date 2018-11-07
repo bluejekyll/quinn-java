6d6a4aae54608bb2336c37d53c343f4a0e3deeb2
bluejekyll <benjaminfry@me.com>
2018-11-07 15:37:30 -0800

    validate the library is working.

--- src/main/java/bluejekyll/quinn/EndpointBuilder.java
+++ src/main/java/bluejekyll/quinn/EndpointBuilder.java
@@ -1,9 +1,13 @@
 package bluejekyll.quinn;
 
+import org.bridj.Pointer;
+
 import bluejekyll.quinn_ffi.Quinn_ffiLibrary;
+import bluejekyll.quinn_ffi.Quinn_ffiLibrary.EndpointBuilderFFI;
 
 public class EndpointBuilder {
     public EndpointBuilder() {
-        // Quinn_ffiLibrary.endpoint_new_builder();
+        Pointer<EndpointBuilderFFI> ptr = Quinn_ffiLibrary.endpointNewBuilder();
+        Quinn_ffiLibrary.endpointBuilderFree(ptr);
     }
 }
